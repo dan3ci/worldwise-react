e532169348704efbb0b6787ef9073aa179c2ad02
dan3ci <everyhomeaakash@gmail.com>
2025-01-07 11:45:00 +0545

    Nested Routes Initiated

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
@@ -13,7 +13,10 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route path="cities" element={<p>List of Cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+        </Route>
         <Route path="login" element={<Login />} />
 
         <Route path="*" element={<PageNotFound />} />
