a214c62be12b42ed42e129c5e7f66db8e8ec5ef5
dan3ci <everyhomeaakash@gmail.com>
2025-01-08 17:17:34 +0545

    City List & COuntry List updated

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,24 @@
+import styles from "./CountryList.module.css";
+import CountryItem from "./CountryItem";
+
+import Spinner from "./Spinner";
+import Message from "./Message";
+
+export default function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length) return <Message message={"Lets visit some cities"} />;
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country} />
+      ))}
+    </ul>
+  );
+}
